1a98c56aa9d1b3425a8f56d825dfd5bd8cbc3b81
Robert <robert_zibert@hotmail.com>
2017-11-12 16:57:20 -0300

    providing more examples

--- src/components/sayHelloName.js
+++ src/components/sayHelloName.js
@@ -1,6 +1,34 @@
 import {h, render, Component, Text} from 'ink'
+import TextInput from 'ink-text-input'
+import {SayHello} from './sayHello'
+import PropTypes from 'prop-types'
 export default class SayHelloName extends Component {
+  static propTypes = {
+    name: PropTypes.string
+  }
+  state = {
+    name: ''
+  }
+
+  handleChange(value) {
+    this.setState(value)
+  }
+
+  handleSubmit() {
+    const unmount = render(<SayHello name={this.state.name} />)
+    unmount()
+  }
+
   render(props, state) {
-    return <Text>Hsello!</Text>
+    return (
+      <div>
+        Enter your name:
+        <TextInput
+          value={state.name}
+          onChange={value => this.handleChange({name: value})}
+          onSubmit={() => this.handleSubmit()}
+        />
+      </div>
+    )
   }
 }
